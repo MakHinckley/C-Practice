f0bfd13c37b9aca460a1466bcba9304af4d12516
Makaila Hinckley <makailamarie95@gmail.com>
2021-07-19 15:48:22 -0500

    practice first commit

---
+++ Practice 1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "Tom";
+            int characterAge;
+            characterAge = 35;
+
+            Console.WriteLine("   /|");
+            Console.WriteLine("  / |");
+            Console.WriteLine(" /  |");
+            Console.WriteLine("/___|");
+
+            Console.WriteLine("There was once a man named " + characterName);
+            Console.WriteLine("He was " + characterAge + " years old");
+
+            characterName = "Mike";
+            Console.WriteLine("He really liked the name " + characterName);
+            Console.WriteLine("But he didn't like being " + characterAge);
+
+            
+            string phrase = " Giraffe Academy";
+            char grade = 'A';
+            int age = 30;
+            double gpa = 3.2;
+            bool isMale = true;
+
+            Console.ReadLine();
+        }
+    }
+}
