bf48deeb91bee029d31f1269fd612b695e3cd0f6
Makaila Hinckley <makailamarie95@gmail.com>
2021-07-21 16:32:02 -0500

    added a small mad lib

--- Game (Mad Libs)/Program.cs
+++ Game (Mad Libs)/Program.cs
@@ -10,6 +10,26 @@ namespace Game__Mad_Libs_
     {
         static void Main(string[] args)
         {
+            string color, pluralNoun, Pluralnoun, celebrity;
+
+            Console.Write("Enter a color: ");
+            color = Console.ReadLine();
+
+            Console.Write("Enter a plural noun: ");
+            pluralNoun = Console.ReadLine();
+
+            Console.Write("Enter a  plural noun: ");
+            Pluralnoun = Console.ReadLine();
+
+            Console.Write("Enter a celebrity: ");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine("Roses are " + color );
+            Console.WriteLine(pluralNoun + " are blue");
+            Console.WriteLine("You love " + Pluralnoun);
+            Console.WriteLine("And I Love " + celebrity);
+
+            Console.ReadLine();
         }
     }
 }
