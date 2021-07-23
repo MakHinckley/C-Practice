b0ac0d4bb1a8a744394a419851889a3e5ba4067e
Makaila Hinckley <makailamarie95@gmail.com>
2021-07-23 14:58:58 -0500

    added some notes on return and if statements

--- C Sharp Notes cont/Program.cs
+++ C Sharp Notes cont/Program.cs
@@ -8,10 +8,87 @@ namespace C_Sharp_Notes_cont
 {
     class Program
     {
+        // Notes:
+        //return statements = returning information back to the callers
+        //If statements:
+        //need a condition and a action that's executed when it's true. (only two possible values)
+        //create a variable for if statements to create a basis for what is true or false.
+        // && (and) let's you use two conditions in the same condition block (are they male and tall?) 
+        //BOTH conditions have to agree, if they dont, they will print the else statement
+        //EX: if a person is both female and tall, then the first condition will be printed. If not, then the second statement will be printed.
+        // || (or) allows there to be only one true condition
+        //EX: if you are a male, but you are tall, the first condition will be ran; same if you were to flip the scenario. IF both are false, then the second condition will run instead.
+        // ! (negation operator) same as python
+
+
+
         static void Main(string[] args)
         {
+            //Return Statements -> creating a cubed power app. Keywords
+            
+            int cubedNumber = Cube(5);
+            Cube(5);
+            //But it doesn't work, Why?? It did return a value, but it needed to be printed in order to be viewed!
+            
+            Console.WriteLine(Cube(5));
+            //can also set it as a variable, put the calling method inside the variable and it'll stay the same!
+           
+            Console.WriteLine(cubedNumber);
+
+            //If statements:
+            bool isFemale = false;
+            bool isTall = false;
+
+            //AND
+
+            if (isFemale && isTall) //<- ONLY gets executed if condition (variable) is TRUE. If you set isFemale to false, it will not execute.
+            {
+                Console.WriteLine("You are a tall female!");
+            }
+            else
+            {
+                Console.WriteLine("You are either not a female, tall or both");
+            }
+
+            //OR
+
+            if (isFemale || isTall) //<- ONLY gets executed if condition (variable) is TRUE. If you set isFemale to false, it will not execute.
+            {
+                Console.WriteLine("Great work! One or more of the conditions has been met!");
+            }
+            else
+            {
+                Console.WriteLine("Both are false! :(");
+            }
+
+            //ELSE IF
+            
+            if (isFemale && isTall) //<- ONLY gets executed if condition (variable) is TRUE. If you set isFemale to false, it will not execute.
+            {
+                Console.WriteLine("Great work! One or more of the conditions has been met!");
+            }
+            else if (isFemale && !isTall)
+            {
+                Console.WriteLine("You are a short female!");
+            }
+            else if (!isFemale && isTall)
+            {
+                Console.Write("You are not a female, but you are tall!");
+            }
+            else
+            {
+                Console.WriteLine("Both are false! :(");
+            }
 
 
+            Console.ReadLine();
         }
+
+        static int Cube(int num) // <- you can return anything! Strings, ints, doubles, arrays etc. 
+        {
+            int result = num * num * num;
+            return result;
+        }
+        
     }
 }
