c66070ced15d5a263e248bdbed64be1ee8df5aab
Makaila Hinckley <makailamarie95@gmail.com>
2021-08-30 12:26:01 -0400

    finished guessing game

--- Guessing Game/Program.cs
+++ Guessing Game/Program.cs
@@ -10,6 +10,43 @@ namespace Guessing_Game
     {
         static void Main(string[] args)
         {
+            string secretWord = "giraffe";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outofGuesses = false; 
+
+
+
+            while (guess != secretWord && !outofGuesses)
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.Write("Enter guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+
+                }
+                else
+                {
+                    outofGuesses = true; 
+                }
+               
+
+            }
+                 if (outofGuesses)
+                 {
+                    Console.Write("You Lose!");
+                 }
+                else
+                {
+                    Console.Write("You Win!");
+                }
+            
+
+
+
+            Console.ReadLine();
         }
     }
 }
